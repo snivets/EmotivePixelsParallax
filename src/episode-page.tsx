f494228dabe46c8261c8f66f9d45f2fc65cf5067
snivets <equinox.nls@gmail.com>
2024-03-04 18:01:02 -0500

    tweaks: style

--- src/episode-page.tsx
+++ src/episode-page.tsx
@@ -4,8 +4,13 @@ import DOMPurify from 'dompurify';
 const EpisodePage = (props: any) => {
   return (
     <motion.div
-      className="full-page relative"
-      style={{ height: '100vh', backgroundImage: 'url(images/' + props.imageFilename + '.jpg)',  backgroundSize: 'cover', backgroundPositionY: 'center' }}
+      className="full-page relative flex items-center"
+      style={{
+        height: '100vh',
+        backgroundImage: 'url(images/' + props.imageFilename + '.jpg)',
+        backgroundSize: 'cover',
+        backgroundPositionY: 'center',
+        backgroundPositionX: 'center', }}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
@@ -13,7 +18,7 @@ const EpisodePage = (props: any) => {
       <div className="absolute bottom-2 left-5 opacity-65 uppercase">
         {props.title}
       </div>
-      <div className="text-4xl p-[20%]" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(props.text)}} />
+      <div className="text-4xl m-[10%] w-full" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(props.text)}} />
     </motion.div>
   )
 }
