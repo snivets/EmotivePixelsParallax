af950ca3a57ac3f047a7efa6d126e74b2866b8f8
snivets <equinox.nls@gmail.com>
2024-03-04 16:50:33 -0500

    fix: point to correct episode-page path

--- src/episode-page.tsx
+++ src/episode-page.tsx
@@ -5,12 +5,12 @@ const EpisodePage = (props: any) => {
   return (
     <motion.div
       className="full-page relative"
-      style={{ height: '100vh', backgroundImage: 'url(/src/images/' + props.imageFilename + '.jpg)',  backgroundSize: 'cover', backgroundPositionY: 'center' }}
+      style={{ height: '100vh', backgroundImage: 'url(images/' + props.imageFilename + '.jpg)',  backgroundSize: 'cover', backgroundPositionY: 'center' }}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
-      <div className="absolute bottom-2 left-5 opacity-65">
+      <div className="absolute bottom-2 left-5 opacity-65 uppercase">
         {props.title}
       </div>
       <div className="text-4xl p-[20%]" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(props.text)}} />
