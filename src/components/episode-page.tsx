67aa814bd93c0215e9d466b441c1b304d1b8ff31
snivets <equinox.nls@gmail.com>
2024-03-04 20:49:00 -0500

    fix: style tweaks, change text

--- src/components/episode-page.tsx
+++ src/components/episode-page.tsx
@@ -18,7 +18,9 @@ const EpisodePage = (props: any) => {
       <div className="absolute bottom-2 left-5 opacity-65 uppercase max-w-[80%] truncate">
         {props.title}
       </div>
-      <div className="text-4xl m-[10%] max-h-[80%] overflow-auto no-scrollbar w-full" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(props.text)}} />
+      <div
+        className="text-4xl m-[10%] max-h-[90vh] w-full"
+        dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(props.text)}} />
     </motion.div>
   )
 }
--- src/components/title-card.tsx
+++ src/components/title-card.tsx
@@ -4,7 +4,13 @@ const TitleCard = () => {
   return (
     <motion.div
       className="full-page"
-      style={{ backgroundColor: 'gray', backgroundImage: 'url(images/epbg.png)', backgroundSize: 'cover', backgroundPositionY: 'center', height: '100vh'}}
+      style={{
+        backgroundColor: 'gray',
+        backgroundImage: 'url(images/epbg.png)',
+        backgroundSize: 'cover',
+        backgroundPositionY: 'center',
+        backgroundPositionX: 'center',
+        height: '100vh'}}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
@@ -12,11 +18,11 @@ const TitleCard = () => {
       <div className="flex justify-center pt-12">
         <img src="images/eplogo.png" alt="Emotive Pixels" className="max-w-[80%]" />
       </div>
-      <div className="mt-12 mx-[10vw] text-3xl">
-        <p>
-          Emotive Pixels is a videogame podcast. We typically discuss (and spoil!) a single game in a way you don't have to play along to enjoy, but sometimes we have multi-game episodes we call Pixel Arrays where we talk about anything we've been enjoying.
+      <div className="mt-12 mx-[10vw] leading-none">
+        <p className="text-3xl leading-none">
+          Emotive Pixels is a videogame podcast. We typically discuss (and spoil!) a single game - playing along is optional. Sometimes, we have multi-game episodes we call Pixel Arrays where we talk around the table about things we've been enjoying.
         </p>
-        <p className="text-xl">
+        <p className="text-xl leading-none">
           We've been at this for ten years, so there are all kinds of episodes in our backlog, often with new guests.
         </p>
       </div>
