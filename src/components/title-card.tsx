02388e1d6f85187a4d119817475b74b4ae88b7d6
snivets <equinox.nls@gmail.com>
2025-01-27 12:03:26 -0500

    Shrink margins for mobile

--- src/components/title-card.tsx
+++ src/components/title-card.tsx
@@ -11,10 +11,10 @@ export function TitleCard() {
         height: '100vh'
       }}
     >
-      <div className="flex justify-center pt-12">
-        <img src="images/eplogo.png" alt="Emotive Pixels" className="max-w-[80%] max-h-[400px]" />
+      <div className="flex justify-center pt-8">
+        <img src="images/eplogo.png" alt="Emotive Pixels" className="max-w-[80%] max-h-[450px]" />
       </div>
-      <div className="mt-12 mx-[10vw] leading-none">
+      <div className="mx-[10vw] leading-none">
         <p className="text-3xl leading-none drop-shadow-[0_5px_5px_rgba(0,0,0,1)]">
           Emotive Pixels is a videogame podcast. We typically discuss (and spoil!) a single game - playing along is optional. Sometimes, we have multi-game episodes we call Pixel Arrays where we talk around the table about things we've been enjoying.
         </p>
